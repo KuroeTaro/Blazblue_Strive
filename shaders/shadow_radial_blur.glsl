a7d6e3af3f9a58656b3329149a3b5729a5d640a0
Xudong Liu <112330682+KuroeTaro@users.noreply.github.com>
2025-07-23 20:52:25 -0700

    Increase sample count in radial blur shaders
    Raised nsamples from 100 to 150 in radial_blur.glsl and from 50 to 100 in shadow_radial_blur.glsl to improve blur quality.

--- shaders/shadow_radial_blur.glsl
+++ shaders/shadow_radial_blur.glsl
@@ -2,7 +2,7 @@ extern vec2 start_coods;
 extern vec2 input_screen_coords;
 extern float blur_start;     //0.5 
 extern float blur_width;     //0.1
-const int nsamples = 50;
+const int nsamples = 100;
 vec4 effect(vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords){
     screen_coords = input_screen_coords;
     vec2 center = start_coods.xy / screen_coords.xy;
